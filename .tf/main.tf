a154688745215d09ee3885c125c4cead0b01e16a
patchcoin-dev <189779141+patchcoin-dev@users.noreply.github.com>
2025-02-20 03:14:40 +0100

    update release actions
    drop coordinator

--- .tf/main.tf
+++ .tf/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  cloud {
+    organization = "patchcoin"
+    workspaces {
+       tags = ["app:patchcoin"]
+    }
+  }
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.49.1"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
 variable "hcloud_token" {
   type      = string
   sensitive = true
@@ -35,27 +51,6 @@ variable "run_id" {
   type = number
 }
 
-variable "existing_ssh_key_name" {
-  description = "Name of the existing SSH key on Hetzner that should be used."
-  type        = string
-  default     = "runner-key"
-}
-
-variable "runner_public_key" {
-  description = "The public SSH key corresponding to the private key (not used if using an existing key)."
-  type        = string
-}
-
-terraform {
-  required_providers {
-    hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = "~> 1.49.1"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
 provider "hcloud" {
   token = var.hcloud_token
 }
@@ -78,8 +73,14 @@ locals {
   ephemeral_runner_token = jsondecode(data.http.github_runner_token.response_body).token
 }
 
-data "hcloud_ssh_key" "runner_key" {
-  name = var.existing_ssh_key_name
+resource "tls_private_key" "default" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "hcloud_ssh_key" "default" {
+  name       = "default-key-${var.run_id}"
+  public_key = tls_private_key.default.public_key_openssh
 }
 
 resource "hcloud_server" "runner" {
@@ -87,7 +88,7 @@ resource "hcloud_server" "runner" {
   name        = "${var.server_name}-${var.run_id}-${count.index}"
   server_type = "cpx51"
   image       = "ubuntu-24.04"
-  ssh_keys    = [data.hcloud_ssh_key.runner_key.id]
+  ssh_keys    = [hcloud_ssh_key.default.id]
 
   user_data = <<-CLOUDINIT
     #cloud-config
@@ -146,7 +147,8 @@ resource "null_resource" "runner_cleanup" {
   count = var.runner_count
 
   triggers = {
-    server_ip = hcloud_server.runner[count.index].ipv4_address
+    server_ip   = hcloud_server.runner[count.index].ipv4_address
+    private_key = tls_private_key.default.private_key_pem
   }
 
   depends_on = [hcloud_server.runner]
@@ -155,10 +157,10 @@ resource "null_resource" "runner_cleanup" {
     when = destroy
 
     connection {
-      host = self.triggers.server_ip
-      type = "ssh"
-      user = "root"
-      private_key = file("./runner_private_key.pem")
+      host        = self.triggers.server_ip
+      type        = "ssh"
+      user        = "root"
+      private_key = self.triggers.private_key
     }
 
     inline = [
